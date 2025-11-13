fced62b6b40145b261ebb81a69f40517b4e0ac70
Charl Concepcion <charliecatxph@gmail.com>
2025-11-14 00:33:33 +0800

    logout failure fix

--- backend/routes/endpoints/logout.js
+++ backend/routes/endpoints/logout.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 
 export default function logout(req, res) {
-    res.clearCookie("refreshToken", {
-        httpOnly: true,
-        secure: process.env.MODE === "PRODUCTION", // Ensures it is only sent over HTTPS
-        sameSite: process.env.MODE === "PRODUCTION" ? "None" : "Lax",
-        path: "/",
-      });
-    res.json({ success: true });
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: process.env.MODE === "PRODUCTION",
+    sameSite: process.env.MODE === "PRODUCTION" ? "None" : "Lax",
+    path: "/",
+    domain:
+      process.env.MODE === "PRODUCTION" ? process.env.SERVER_URL : "localhost",
+  });
+  res.json({ success: true });
 }
